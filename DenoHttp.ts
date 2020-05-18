9d8bb8b8734987fc3e7364a1396d29e7ae6b0dae
Balaji S Prabakaran <balaji.prabakaran@gmail.com>
2020-05-18 23:46:45 +0530

    http

---
+++ DenoHttp.ts
@@ -0,0 +1,7 @@
+import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
+
+const server = serve({ port: 8000 });
+for await (const req of server) {
+  console.log("Deno web");
+  req.respond({body:'message from deno'});
+}
