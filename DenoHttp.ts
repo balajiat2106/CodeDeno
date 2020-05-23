7b5c64fa010fe4a20c9fecc3cd77d0635e8f7447
Balaji S Prabakaran <balaji.prabakaran87@gmail.com>
2020-05-23 23:08:05 +0530

    Temp Commit 1

--- DenoHttp.ts
+++ DenoHttp.ts
@@ -2,6 +2,6 @@ import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 
 const server = serve({ port: 8000 });
 for await (const req of server) {
-  console.log("Deno web sample");
+  console.log("Deno web sample done");
   req.respond({body:'message from deno'});
 }
